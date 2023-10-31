d9539e0f997feff40e67822ca821eeeffaef00b8
Matheus Santana <matheussantanaoli@gmail.com>
2023-10-30 22:14:56 -0300

    feat: added postservice to controller

--- src/http/post/post.controller.ts
+++ src/http/post/post.controller.ts
@@ -3,14 +3,16 @@ import {
   Controller,
   Get,
   Post,
+  Req,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { PostService } from './post.service';
-import { Public } from 'src/decorators/is-public.decorator';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreatePostDto } from './dto/create-post-dto';
 import { UploadService } from 'src/http/upload/upload.service';
+import { Public } from 'src/decorators/is-public.decorator';
+import { Request } from 'express';
 
 @Controller('post')
 export class PostController {
@@ -19,15 +21,15 @@ export class PostController {
     private readonly postService: PostService,
   ) {}
 
-  @Public()
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async create(
     @UploadedFile() file: Express.Multer.File,
     @Body() createPostDto: CreatePostDto,
+    @Req() req: Request,
   ) {
     await this.uploadService.create(file);
-    await this.postService.createPost(file, createPostDto);
+    await this.postService.createPost(file, createPostDto, req);
   }
 
   @Public()
