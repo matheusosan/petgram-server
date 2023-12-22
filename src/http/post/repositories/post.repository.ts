4ca0ffd0609e932c94993eae53c015c966085001
Matheus Santana <matheussantanaoli@gmail.com>
2023-12-22 00:57:13 -0300

    feat: implemented repository pattern to the user service

---
+++ src/http/post/repositories/post.repository.ts
@@ -0,0 +1,82 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+import { S3Service } from 'src/http/s3/s3.service';
+
+@Injectable()
+export class PostRepository {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly s3service: S3Service,
+  ) {}
+
+  async create(filename: string, url: string, description: string, id: number) {
+    try {
+      const post = await this.prisma.post.create({
+        data: {
+          filename,
+          photoUrl: url,
+          description,
+          authorId: Number(id),
+        },
+      });
+      return post;
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          throw new NotFoundException('Post não encontrado', { cause: e });
+        }
+      }
+      throw e;
+    }
+  }
+
+  async getAllPosts() {
+    try {
+      const data = await this.prisma.post.findMany({
+        select: {
+          id: true,
+          description: true,
+          filename: true,
+          photoUrl: true,
+          createdAt: true,
+          author: {
+            select: {
+              username: true,
+            },
+          },
+        },
+      });
+
+      return data;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException(
+            'Posts não encontrados ou não existentes.',
+          );
+        }
+        throw error;
+      }
+    }
+  }
+
+  async deletePost(id: string) {
+    try {
+      const postToDelete = await this.prisma.post.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+
+      await this.s3service.delete(postToDelete.filename);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          throw new NotFoundException('Post não encontrado', { cause: e });
+        }
+      }
+      throw e;
+    }
+  }
+}
