405381a5512819fc06738470c5b7278c8e5adc6c
Matheus Santana <matheussantanaoli@gmail.com>
2023-12-22 00:54:44 -0300

    feat: implemented validations to the create user dto

--- src/http/user/dto/create-user.dto.ts
+++ src/http/user/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, Min } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Min(3, {
+    message: 'O nome de usuário precisa conter no mínimo 2 caractéres.',
+  })
   username: string;
+  @IsString()
+  @IsNotEmpty()
+  @Min(5, { message: 'A senha precisa conter no mínimo 5 caractéres.' })
   password: string;
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
