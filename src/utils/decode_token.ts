1b9d67c8c075313bad2e7382d5cdac7e804f401e
Matheus Santana <matheussantanaoli@gmail.com>
2023-12-22 01:00:09 -0300

    chore: added null token verification

--- src/utils/decode_token.ts
+++ src/utils/decode_token.ts
@@ -1,11 +1,13 @@
 import * as jwt from 'jsonwebtoken';
 
 export const decode_token = (token: string) => {
-  const splitedToken = token.split(' ')[1];
+  if (!token) {
+    throw new Error('Você deve estar logado');
+  }
 
   const secret = process.env.JWT_SECRET;
 
-  const payload = jwt.verify(splitedToken, secret);
+  const payload = jwt.verify(token, secret);
 
   return {
     id: Number(payload.sub),
