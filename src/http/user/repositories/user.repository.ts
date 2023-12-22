cad56b5430ef05d70b87de158e223797fd8d6101
Matheus Santana <matheussantanaoli@gmail.com>
2023-12-22 00:54:05 -0300

    feat: implemented the repository pattern in user

---
+++ src/http/user/repositories/user.repository.ts
@@ -0,0 +1,95 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(email: string, username: string, hashPassword: string) {
+    try {
+      const createdUser = await this.prisma.user.create({
+        data: {
+          email,
+          password: hashPassword,
+          username,
+        },
+      });
+
+      return {
+        ...createdUser,
+        password: undefined,
+      };
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ConflictException('Usuário já existe.');
+        }
+      }
+      throw new Error('Ocorreu um erro: ' + e);
+    }
+  }
+
+  async findByEmail(email: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      return user;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async findByUsername(username: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          username,
+        },
+      });
+
+      return {
+        ...user,
+        password: undefined,
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getUserAndPosts(id: number) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id,
+        },
+        select: {
+          username: true,
+          id: true,
+          posts: {
+            select: {
+              description: true,
+              id: true,
+              photoUrl: true,
+            },
+          },
+        },
+      });
+      return user;
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          throw new NotFoundException('Usuário não encontrado.');
+        }
+      }
+      throw new Error('Ocorreu um erro: ' + e);
+    }
+  }
+}
