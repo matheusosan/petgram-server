b803d269096fbd0d2d710fd753cfc3faa75a6927
Matheus Santana <matheussantanaoli@gmail.com>
2024-02-09 18:11:05 -0300

    adjsut imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './database/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './http/user/user.module';
 import { AuthModule } from './http/auth/auth.module';
 import { PostModule } from './http/post/post.module';
+import { FollowModule } from './http/follow/follow.module';
+import { PrismaModule } from './http/database/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
+    FollowModule,
     AuthModule,
     PostModule,
+    PrismaModule,
   ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
